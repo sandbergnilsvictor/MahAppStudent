ac38d623577d44b214955ca70d20013277201654
sandbergnilsvictor <sandbergvictor@yahoo.se>
2013-10-31 14:15:35 +0100

    Add style to schedule list and added functionaly to settingsActivity

--- src/se/mah/kd330a/project/settings/view/SettingsActivity.java
+++ src/se/mah/kd330a/project/settings/view/SettingsActivity.java
@@ -1,7 +1,11 @@
 package se.mah.kd330a.project.settings.view;
 
+import java.util.ArrayList;
+
 import se.mah.kd330a.project.R;
 import se.mah.kd330a.project.adladok.test.AdLadokTest;
+import se.mah.kd330a.project.adladok.model.Me;
+import se.mah.kd330a.project.adladok.model.Course;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,20 +14,48 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
 	private static final String PREFS_NAME
 	    = "myCourses";
 	SharedPreferences sharedPref;
+	private String profilName;
+	private String profilId;
+	private ArrayList<Course> myCourses;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_help);
-		
+		setContentView(R.layout.activity_settings);
+		addProfilInformation();
 	
 	}
+	
+
+	private void addProfilInformation() {
+		if(Me.getUserID() != null) {
+			profilName = Me.getDispayName();
+			profilId = Me.getUserID();
+			myCourses = (ArrayList<Course>) Me.getCourses();
+			TextView profilNameTextView = (TextView) findViewById(R.id.profil_name);
+			TextView profilIdTextView = (TextView) findViewById(R.id.profil_id);
+			profilNameTextView.setText(profilName);
+			profilIdTextView.setText(profilId);
+			LinearLayout coursesContent = (LinearLayout) findViewById(R.id.my_courses_content);
+			for(Course course: myCourses) {
+				TextView currentCourse = new TextView(this);
+				currentCourse.setText(course.getDisplaynameSv());
+				coursesContent.addView(currentCourse);
+			}
+		} else {
+			TextView profilNameTextView = (TextView) findViewById(R.id.profil_name);
+			profilNameTextView.setText("No profile");
+		}
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,7 +64,7 @@ public class SettingsActivity extends Activity {
 		return true;
 	}
 	
-	public void toSearchable(View view) {
+	public void toLadokInlogg(View view) {
 		Intent intent = new Intent(this, AdLadokTest.class);
 		startActivity(intent);
 		
