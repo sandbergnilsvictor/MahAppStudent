e75653e33bb68c9a58f5dd5f5e4ef4f59a62f36c
Klari <klara.kutas@yahoo.com>
2013-11-05 13:35:15 +0100

    db changes

--- src/se/mah/kd330a/project/find/view/FragmentBuilding.java
+++ src/se/mah/kd330a/project/find/view/FragmentBuilding.java
@@ -1,8 +1,6 @@
 package se.mah.kd330a.project.find.view;
 
 import java.util.Arrays;
-import java.util.List;
-
 import se.mah.kd330a.project.R;
 import se.mah.kd330a.project.find.data.ImageLoader;
 import android.content.Intent;
@@ -38,7 +36,7 @@ public class FragmentBuilding extends Fragment {
 		
 		buildingCode = args.getString(ARG_BUILDING);
 		String[] buildings = getResources().getStringArray(R.array.find_building_code_array);
-		int pos = Arrays.asList(buildings).indexOf(buildingCode);
+		final int pos = Arrays.asList(buildings).indexOf(buildingCode);
 		
 		buildings = getResources().getStringArray(R.array.find_building_array);
 		((TextView) getView().findViewById(R.id.text_find_startTitle)).setText(buildings[pos]);
@@ -73,10 +71,8 @@ public class FragmentBuilding extends Fragment {
 
 			@Override
 			public void onClick(View v) {
-				List<String> buildingCodes = Arrays.asList(getResources().getStringArray(R.array.find_building_code_array));
-				int buildingPos = buildingCodes.indexOf(buildingCode);
 				String[] buildingNames = getResources().getStringArray(R.array.find_building_array);
-				String location = buildingNames[buildingPos];
+				String location = buildingNames[pos];
 
 				if(location.equals("Klerken (Kl)"))
 					location = "Jan Waldenströms gata 25";
