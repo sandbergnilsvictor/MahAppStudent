caf1becffd360ee9631f95cdad6319043cc7da86
mynario <mynario@gmail.com>
2013-11-07 17:29:22 +0100

    ondra schedule repair
    weeks number reparation

--- src/se/mah/kd330a/project/schedule/data/ParseData.java
+++ src/se/mah/kd330a/project/schedule/data/ParseData.java
@@ -1,6 +1,7 @@
 package se.mah.kd330a.project.schedule.data;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -70,6 +71,13 @@ public class ParseData {
 
 	private ScheduleWeek getScheduleWeek(int weekFromThisWeek) {
 		ScheduleWeek scheduleWeek = new ScheduleWeek();
+		int displayedWeek=0; 
+		
+		Calendar calendarForWeek=Calendar.getInstance();
+				
+		displayedWeek=calendarForWeek.get(Calendar.WEEK_OF_YEAR)+weekFromThisWeek-1;
+		scheduleWeek.setWeekNumber(displayedWeek % 52 +1);
+
 		scheduleWeek.setWeekNumber(54+weekFromThisWeek);
 		ArrayList<ScheduleItem> thisWeekList = new ArrayList<ScheduleItem>();
 		Collection<?> kronox_events = KronoxCalendar
